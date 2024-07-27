92189ad200d074375dcbe358e8a6c8bf79407f81
German Morales <germanwebdeveloper@gmail.com>
2024-07-27 17:55:53 +0200

    prisma database configuration & migrations

---
+++ prisma/data/categories.ts
@@ -0,0 +1,32 @@
+export const categories = [
+    {
+        slug: "cafe",
+        name: "Café",
+        is_active: true
+    },
+    {
+        slug: "hamburguesa",
+        name: "Hamburguesas",
+        is_active: true
+    },
+    {
+        slug: "pizza",
+        name: "Pizzas",
+        is_active: true
+    },
+    {
+        slug: "dona",
+        name: "Donas",
+        is_active: true
+    },
+    {
+        slug: "pastel",
+        name: "Pasteles",
+        is_active: true
+    },
+    {
+        slug: "galletas",
+        name: "Galletas",
+        is_active: true
+    }
+];
---
+++ prisma/migrations/20240727141033_added_is_active/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT true;
