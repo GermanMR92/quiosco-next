d369125762811bf9a338da00f74c2551ae381110
German Morales <germanwebdeveloper@gmail.com>
2024-07-28 14:17:33 +0200

    Calling the database products in the dynamic route | Showing categories icon o sidebar

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,22 +1,26 @@
 import { prisma } from '@/src/lib/prisma'
+import CategoryIcon from '../ui/CategoryIcon';
 
 async function getCatetories() {
   return prisma.category.findMany({
-    where: {
-      is_active: true
-    }
+    where: {is_active: true}
   })
 }
 
 export default async function OrderSidebar() {
 
   const categories = await getCatetories();
-  console.log(categories);
-  
 
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-      OrderSidebar
+      <nav className='mt-10'>
+        {categories.map( (category => (
+          <CategoryIcon 
+            key={category.id}
+            category={category}
+          />
+        )))}
+      </nav>
     </aside>
   )
 }
