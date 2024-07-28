f0da34040b8e7b932d89dd8fc887d1d7b89cf32a
German Morales <germanwebdeveloper@gmail.com>
2024-07-28 12:46:03 +0200

    Import some svg and imagens for products & categories

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,7 +1,22 @@
-export default function OrderSidebar() {
+import { prisma } from '@/src/lib/prisma'
+
+async function getCatetories() {
+  return prisma.category.findMany({
+    where: {
+      is_active: true
+    }
+  })
+}
+
+export default async function OrderSidebar() {
+
+  const categories = await getCatetories();
+  console.log(categories);
+  
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-        OrderSidebar
+      OrderSidebar
     </aside>
   )
 }
---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+// It is responsible for opening and reusing prisma connections.
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
