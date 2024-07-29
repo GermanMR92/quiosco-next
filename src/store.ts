6af07eab74c97cda5bebd32cc8881d7bddd170d9
German Morales <germanwebdeveloper@gmail.com>
2024-07-29 20:47:38 +0200

    Avoid duplicates when adding to cart

--- src/store.ts
+++ src/store.ts
@@ -7,20 +7,45 @@ interface Store {
     addToOrder: (product: Product) => void;
 }
 
-export const useStore = create<Store>((set) => ({ // We ned set to modify the state
+/**
+ * Represents the store state.
+ */
+export const useStore = create<Store>((set, get) => ({
     order: [],
-    addToOrder: (product) => {
 
+    /**
+     * Adds a product to the order.
+     * @param product - The product to be added.
+     */
+    addToOrder: (product) => {
         // We extract unnecessary values
         const { categoryId, image, description, is_active, ...data } = product;
 
-        // We modify the state with the new product
-        set((state) => ({
-            order: [...state.order, {
-                ...data,
-                quantity: 1,
-                subtotal: 1 * product.price
-            }]
+        let order: OrderItem[] = [];
+
+        if (get().order.find((item) => item.id === product.id)) {
+            order = get().order.map((item) => item.id === product.id ?
+                {
+                    ...item,
+                    quantity: item.quantity + 1,
+                    subtotal: (item.quantity + 1) * product.price,
+                }
+                : item
+            );
+        } else {
+            order = [
+                ...get().order,
+                {
+                    ...data,
+                    quantity: 1,
+                    subtotal: 1 * product.price,
+                },
+            ];
+        }
+
+        // We modify the state with the new order
+        set(() => ({
+            order,
         }));
-    }
+    },
 }));
