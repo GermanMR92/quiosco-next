f4845592c57eb2d11833276da56c3a37961144f9
German Morales <germanwebdeveloper@gmail.com>
2024-08-25 19:51:01 +0200

    fix: Update grid layout and max width on OrderPage

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -24,7 +24,7 @@ export default async function OrderPage({ params }: { params: { category: string
         Elige y personaliza tu pedido a continuación
       </Heading>
 
-      <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-5 gap-4 items-start max-w-[1980px] mx-auto'>
         {products.map(product => (
           <ProductCard
             key={product.id}
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { formatCurrency } from '@/src/utils'
+import { formatCurrency, getImagePath } from '@/src/utils'
 import { Product } from '@prisma/client'
 import AddProductButton from './AddProductButton' // Client component
 
@@ -10,13 +10,16 @@ type ProductCardProps = {
 
 // TODO: Hacer que las imagenes encajen en cualquier resolucion (FHD o superior)
 export default function ProductCard({ product }: ProductCardProps) {
+
+    const imagePath = getImagePath(product.image)
+
     return (
         <div className='rounded border bg-white overflow-auto flex flex-col justify-between h-full'>
 
             <Image
                 width={400}
                 height={500}
-                src={`/products/${product.image}.jpg`}
+                src={imagePath}
                 alt={`Imagen ${product.name}`}
             />
 
